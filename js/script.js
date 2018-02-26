4d544c9980b32f6313db2adf7e8c330fb88344d8
Tomasz Paterka <tomekpaterka@gmail.com>
2018-02-26 18:27:05 +0100

    Add buttons to slide carousel

--- js/script.js
+++ js/script.js
@@ -1,34 +1,46 @@
-$(function(){
+$(function () {
 	var carouselList = $("#carousel ul");
-	function changeSlide() {
-		carouselList.animate({'marginLeft':-600}, 500, moveFirstSlide);
+
+	function changeSlideLeft() {
+		carouselList.animate({
+			'marginLeft': -1200
+		}, 500, moveFirstSlide);
 	};
+
 	function moveFirstSlide() {
 		var firstItem = carouselList.find("li:first");
 		var lastItem = carouselList.find("li:last");
 		lastItem.after(firstItem);
-		carouselList.css({marginLeft:0});
+		carouselList.css({
+			marginLeft: -600
+		});
 	};
-	setInterval(changeSlide, 3000);
+	function changeSlideRight() {
+		carouselList.animate({
+			'marginLeft': 0
+		}, 500, moveLastSlide);
+	};
+
+	function moveLastSlide() {
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+		firstItem.before(lastItem);
+		carouselList.css({
+			marginLeft: -600
+		});
+	};
+	//setInterval(changeSlide, 3000);
+	var controlPrev = $("#carousel-control-prev");
+	controlPrev.click(function () {
+		changeSlideLeft();
+	});
+	var controlNext = $("#carousel-control-next");
+	controlNext.click(function () {
+		changeSlideRight();
+	});
 });
 
-/*var slideIndex = 1;
-showSlides(slideIndex);
 
-function plusSlides(n) {
-	showSlides(slideIndex += n);
-}
 
-function showSlides(n) {
-	var slides = document.getElementsByClassName("mySlides");
-	if (n > slides.length) {
-		slideIndex = 1
-	}
-	if (n < 1) {
-		slideIndex = slides.length
-	}
-	for (i = 0; i < slides.length; i++) {
-		slides[i].style.display = "none";
-	}
-	slides[slideIndex - 1].style.display = "block";
-}*/
+
+
